9091ef2b7254d3dc09f957f3602aa6033d2ede3c
gustavo <gustavo.az.ol@gmail.com>
2019-08-18 10:35:06 -0300

    Ajustes de arquitetura
    Ajustando hierarquia dos diretorios.
    Removendo storyboard e iniciando controller por código

---
+++ RepoStars/AppFlows/RepoListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RepoListViewController.swift
+//  RepoStars
+//
+//  Created by Gustavo Azevedo de Oliveira on 18/08/19.
+//  Copyright © 2019 Gustavo Azevedo de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class RepoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
